d38eecfa6af0122725aa274a49a736318296cdf6
Matt <58913953+ms5984@users.noreply.github.com>
2021-06-07 17:50:59 -0400

    Add license info and javadoc

--- src/main/java/com/github/sanctum/templates/Template.java
+++ src/main/java/com/github/sanctum/templates/Template.java
@@ -1,3 +1,18 @@
+/*
+ *   Copyright 2021 Sanctum <https://github.com/the-h-team>
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
 package com.github.sanctum.templates;
 
 import com.google.common.collect.ImmutableList;
@@ -18,6 +33,21 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Supplier;
 
+/**
+ * Describe various abstract properties to set of any item processed
+ * by this class:
+ *
+ * <ul>
+ *     <li>Display name</li>
+ *     <li>Lore</li>
+ *     <li>Item count</li>
+ *     <li>Enchantments and their levels</li>
+ *     <li>ItemFlags to add</li>
+ *     <li>ItemFlags to remove</li>
+ * </ul>
+ *
+ * @author ms5984
+ */
 @SerializableAs("SanctumTemplate")
 @DelegateDeserialization(SimpleTemplate.class)
 public interface Template extends ConfigurationSerializable {
